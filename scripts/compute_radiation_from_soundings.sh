581702a159d175e8cbcbd56fdd0de3a8b86cacf9
Benjamin Fildier <bfildier.work@gmail.com>
2020-02-06 16:17:46 -0400

    create wrapper script compute_radiation_from_soundings.sh

--- scripts/compute_radiation_from_soundings.sh
+++ scripts/compute_radiation_from_soundings.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
 
+# by Ben Fildier and Ludovic Touze-Peiffer
+# Uses radiative transfer code by Robert Pincus to calculate
+# radiative profiles from HALO dropsonde data
+
+sonde_path=${1%\/*}
+sonde_filename=${1##*\/}
+echo 'processing sonde '$sonde_filename
+rad_filename=${sonde_filename%*.nc}'_rad.nc'
 
 # Merge dropsonde profiles with standard atmosphere profiles
-python compute-radiation-from-soundings.py --sonde_file ${sonde_filename}
+echo 'merging sounding and reference profiles in new file '$rad_filename
+python compute-radiation-from-soundings.py --sonde_file ${sonde_path}/${sonde_filename}
 
 # Compute radiative profles
+echo "compute radiative profiles and append them to ${rad_filename}" 
+../code/sounding_profile ${rad_filename}
 
-
-
+echo "move ${rad_filename} to ${sonde_path}"
+mv ${rad_filename} ${sonde_path}/
