d61161e14cd67492dd1a3ef7ec071caccdfbcf46
ludovictouzepeiffer <ludovic.touzepeiffer@gmail.com>
2020-06-22 08:28:55 +0200

    test

--- scripts/combine_reference_and_sonde_profiles.py
+++ scripts/combine_reference_and_sonde_profiles.py
@@ -24,6 +24,7 @@ deltaZ = 8.62                     #in m corresponding to deltaP=100Pa for T=25°
 #
 
 def combine_sonde_and_background(all_sondes_file, background_file, SST_dir, deltaP=100, sfc_emis=.98, sfc_alb=0.07, mu0=1., ghgs=ghgs):
+    
     #
     # Maybe someone can show me how to use Python doc strings?
     #   This routine takes a single background sounding extracted from the Garand atmospheres distributed with RTE+RRTMGP
@@ -53,7 +54,7 @@ def combine_sonde_and_background(all_sondes_file, background_file, SST_dir, delt
     number_sondes = len(all_sondes.launch_time)
     
     for i in range(number_sondes):
-   # for i in range(100,106):
+    #for i in range(100,106):
 
         alt_var = "height"
         p_var = 'pressure'
@@ -80,7 +81,7 @@ def combine_sonde_and_background(all_sondes_file, background_file, SST_dir, delt
         # Units conversion
         #
             sonde[p_var] = sonde[p_var] * PaTohPa       # hPa to Pa
-            sonde[q_var] = sonde[q_var] * gtokg / epsilon # Mass to volume mixing ratio
+            sonde[q_var] = sonde[q_var] / epsilon # Mass to volume mixing ratio
             sonde[t_var] = sonde[t_var] + CtoK          # C to K
     
     
@@ -113,6 +114,8 @@ def combine_sonde_and_background(all_sondes_file, background_file, SST_dir, delt
             
             back_zlay = back.swap_dims({'lay':'p_lay'}).reset_coords().zlay.interp(p_lay=back_plays)
             back_zlay = back_zlay + sonde[alt_var].max().values - back_zlay.min().values + 100
+            
+            print(sonde[alt_var].max().values)
             
             zlay = np.append(back_zlay,sonde[alt_var].interp({p_var:sonde_plays}))
             zlev = np.append(np.append(back.zlev.max(), 0.5*(zlay[1:] + zlay[:-1])), zlay.min() - deltaZ/2)
--- scripts/compute_radiation_from_soundings.sh
+++ scripts/compute_radiation_from_soundings.sh
@@ -12,8 +12,8 @@ cdir=${wdir}/code
 
 #---- Ludo
 
-# ifile=/run/media/ludo/DATA/google-drive/Thèse/EUREC4a/github/Input/all_sondes.nc
-# odir=${wdir}/output/rad_profiles
+ifile=/run/media/ludo/DATA/google-drive/Thèse/EUREC4a/github/Input/all_sondes_2.nc
+odir=${wdir}/output/rad_profiles
 #ifile=/run/media/ludo/DATA/google-drive/Thèse/EUREC4a/github/Input/Dropsondes/all_dropsondes.nc
 #odir=${wdir}/output/rad_profiles_dropsondes
 
@@ -22,20 +22,20 @@ cdir=${wdir}/code
 #odir=${wdir}/output/rad_profiles_radiosondes
 #ifile=/Users/bfildier/Data/EUREC4A/merged/sondes/all_dropsondes.nc
 #odir=${wdir}/output/rad_profiles_dropsondes
-ifile=/Users/bfildier/Data/EUREC4A/merged/sondes/proxy_sondes_fixedT.nc
-odir=${wdir}/output/rad_profiles
+#ifile=/Users/bfildier/Data/EUREC4A/merged/sondes/proxy_sondes_fixedT.nc
+#odir=${wdir}/output/rad_profiles
 
 mkdir -p ${odir}
 
 python combine_reference_and_sonde_profiles.py --sonde_file=${ifile} --out_dir=${odir}
 
-for ofile in `ls ${odir}/*.nc`; do
-              echo 'Compute radiation profile '$ofile
-              ${cdir}/sonde_radiation $ofile
-              echo " "
-done
-
-python post_processing.py --in_dir=${odir} --out_dir=$odir --comp_qrad=True
+#for ofile in `ls ${odir}/*.nc`; do
+#              echo 'Compute radiation profile '$ofile
+#              ${cdir}/sonde_radiation $ofile
+#              echo " "
+#done
+#
+#python post_processing.py --in_dir=${odir} --out_dir=$odir --comp_qrad=True
 
 #To compute the radiative heating itself, run instead
 
